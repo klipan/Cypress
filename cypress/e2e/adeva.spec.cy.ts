4913e8f41f94b8207e80f65e014666cc5128706b
klipan <nikola.klipa@yahoo.com>
2025-07-28 15:41:39 +0200

    POM upd

--- cypress/e2e/adeva.spec.cy.ts
+++ cypress/e2e/adeva.spec.cy.ts
@@ -1,14 +1,22 @@
-describe('pizza nutella', () => {
-    it('limun tart', () => {
-      cy.visit('/');
-      cy.get('#search-query').clear().type('hammer');
-      cy.get('[data-test="search-submit"]').click();
-      cy.get('a').contains('Thor Hammer').click();
-      cy.get('[data-test="product-name"]').should('have.text','Thor Hammer');
-      cy.get('[data-test="quantity"]').click();
-      cy.get('[data-test="quantity"]').type('10');
-      cy.get('[data-test="quantity"]').clear().type('10');
-      cy.get('[data-test="quantity"]').clear().type('11');
-      cy.get('[data-test="add-to-cart"]').click();
+import ProductSelection from '../support/pageObjects/ProductSelection';
+import quantity from '../support/pageObjects/quantity';
+
+describe('autotest', () => {
+    it('search product', () => {
+        ProductSelection.visit('/');
+        ProductSelection.ime('Thor Hammer')
+        ProductSelection.search();
+        ProductSelection.select('Thor Hammer');
+        ProductSelection.verify('Thor Hammer');
+        quantity.quantity(10);
+        quantity.addToCart();
+        quantity.verifyMessage(' You can only have one Thor Hammer in the cart. ')
+
+
+    //  cy.get('[data-test="quantity"]').click();
+    //  cy.get('[data-test="quantity"]').type('10');
+     // cy.get('[data-test="quantity"]').clear().type('10');
+     // cy.get('[data-test="quantity"]').clear().type('11');
+     // cy.get('[data-test="add-to-cart"]').click();
     })
   })
